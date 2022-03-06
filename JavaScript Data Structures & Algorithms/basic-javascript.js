0d9c7903098f07a591708b9a72c6af111cb31f0f
Jax-ai <jacques@tesseract-cloud.co.za>
2022-03-06 02:28:07 +0200

    Now working on Objects

--- JavaScript Data Structures & Algorithms/basic-javascript.js
+++ JavaScript Data Structures & Algorithms/basic-javascript.js
@@ -961,3 +961,111 @@ function cc(card) {
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+/*
+~~~~~~~~~~~~~~~~~~~ Objects ~~~~~~~~~~~~~~~~~~~~
+*/
+const myDog = {
+  // Only change code below this line
+  name: "Fluffy",
+  legs: 4,
+  tails: 1,
+  friends: ["john", "mat"]
+
+
+  // Only change code above this line
+};
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Accessing Object Properties with Dot Notation ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Accessing Object Properties with Bracket Notation ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const testObj2 = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj2['an entree'];   // Change this line
+const drinkValue = testObj2['the drink'];    // Change this line
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Accessing Object Properties with Variables~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const testObj3 = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Updating Object Properties ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myDog4 = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog["name"] = "Happy Coder";
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Add New Properties to a JavaScript Object ~~~~~~~~~~~~~~~~~~~~
+*/
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+
+/*
+~~~~~~~~~~~~~~~~~~~ Delete Properties from a JavaScript Object ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
+
+/*
+~~~~~~~~~~~~~~~~~~~ Using Objects for Lookups ~~~~~~~~~~~~~~~~~~~~
+*/
