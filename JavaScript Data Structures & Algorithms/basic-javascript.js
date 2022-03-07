6689d2789d4133479b677ea9ea3f288ef698dfc2
Jax-ai <jacques@tesseract-cloud.co.za>
2022-03-07 22:50:44 +0200

    Testing Objects for Properties

--- JavaScript Data Structures & Algorithms/basic-javascript.js
+++ JavaScript Data Structures & Algorithms/basic-javascript.js
@@ -1091,3 +1091,19 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Using Objects for Lookups ~~~~~~~~~~~~~~~~~~~~
+*/
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+
+  if(obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+
+  // Only change code above this line
+}
