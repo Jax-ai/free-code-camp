076c87f57418a951e17a11f12cbcdf1462d48428
Jax-ai <jacques@tesseract-cloud.co.za>
2022-03-07 23:03:13 +0200

    Nested objects and arrays

--- JavaScript Data Structures & Algorithms/basic-javascript.js
+++ JavaScript Data Structures & Algorithms/basic-javascript.js
@@ -1135,3 +1135,47 @@ const myMusic = [
     ],
   }
 ];
+
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Accessing Nested Objects ~~~~~~~~~~~~~~~~~~~~
+*/
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Accessing Nested Arrays ~~~~~~~~~~~~~~~~~~~~
+*/
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
