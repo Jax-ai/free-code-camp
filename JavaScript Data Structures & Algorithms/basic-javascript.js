1b215c5f3c95af09a92fd610fc7aab07d51122c2
Jax-ai <jacques@tesseract-cloud.co.za>
2022-03-08 23:12:45 +0200

    Completed the profile lookup challenge

--- JavaScript Data Structures & Algorithms/basic-javascript.js
+++ JavaScript Data Structures & Algorithms/basic-javascript.js
@@ -1236,6 +1236,188 @@ updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 
 
 
+/*
+~~~~~~~~~~~~~~~~~~~ Iterate with JavaScript While Loops ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myArray = [];
+
+let i = 5;
+while (i >= 0) {
+
+  myArray.push(i);
+  i--;
+}
+console.log(myArray);
+// Only change code below this line
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Iterate with JavaScript For Loops ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myArray = [];
+
+for (let i = 1;i <= 5; i++) {
+
+  myArray.push(i);
+
+}
+
+// Only change code below this line
+
+/*
+~~~~~~~~~~~~~~~~~~~ Iterate Odd Numbers With a For Loop ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myArray = [];
+
+for (let i = 1; i <= 9; i += 2){
+
+  myArray.push(i);
+
+}
+
+// Only change code below this line
+
+/*
+~~~~~~~~~~~~~~~~~~~ Count Backwards With a For Loop ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myArray = [];
+
+for (let i = 9; i >= 1; i -= 2) {
+
+  myArray.push(i);
+}
+
+// Only change code below this line
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Iterate Through an Array with a For Loop ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+
+  total += myArr[i];
+}
+
+// Only change code below this line
 
 
 
+/*
+~~~~~~~~~~~~~~~~~~~ Nesting For Loops ~~~~~~~~~~~~~~~~~~~~
+*/
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+
+  for(let i = 0; i < arr.length; i++) {
+
+    for(let j = 0; j < arr[i].length; j++) {
+       product *= arr[i][j];
+      }
+  }
+
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Iterate with JavaScript Do...While Loops ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const myArray = [];
+let ii = 10;
+
+// Only change code below this line
+
+do {  
+  myArray.push(i);
+  i++;
+}while (i < 5) 
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Replace Loops using Recursion ~~~~~~~~~~~~~~~~~~~~
+*/
+function sum(arr, n) {
+  // Only change code below this line
+  if(n <= 0){
+    return 0;
+  } else {
+    return arr[n-1] + sum(arr, n-1);
+  }
+
+  // Only change code above this line
+}
+
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Profile Lookup ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  console.log("\n");
+  for (let i = 0; i < contacts.length; i++) {
+
+    if(contacts[i].firstName == name) {
+
+      if(contacts[i].hasOwnProperty([prop])) {
+
+        return contacts[i][prop];
+        console.log("property found");
+      } else {
+
+        return "No such property";
+      }
+      
+    }
+  } 
+  
+  return "No such contact"; 
+  
+  
+
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
