45a96ec132b5d38216fedfd9ec83c3c6415248fc
Jax-ai <jacques@tesseract-cloud.co.za>
2022-03-08 21:50:17 +0200

    Completed the records problem

--- JavaScript Data Structures & Algorithms/basic-javascript.js
+++ JavaScript Data Structures & Algorithms/basic-javascript.js
@@ -1179,3 +1179,63 @@ const myPlants = [
 ];
 
 const secondTree = myPlants[1].list[1];
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Tracks ~~~~~~~~~~~~~~~~~~~~
+*/
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+
+  if(value == '') {    
+     delete records[id][prop];
+  
+  } else if(prop == 'tracks') {   
+
+
+    if(records[id].hasOwnProperty('tracks')) {      
+      records[id].tracks.push(value);   
+
+    } else {
+      records[id].tracks = [];
+      records[id].tracks.push(value);       
+    }
+  //console.log(records[id].hasOwnProperty(tracks));
+
+
+  } else {
+    
+    records[id][prop] = value;
+  }
+
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+
+
+
+
+
