4f46706296757faa527f4a2baecf6e04d296d00c
Jax-ai <jacques@tesseract-cloud.co.za>
2022-03-09 02:43:18 +0200

    Nested objects

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -81,3 +81,56 @@ const increment = (number, value = 1) => number + value;
 const sum = (...args) => {
     return args.reduce((a, b) => a + b, 0);
   }
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Use the Spread Operator to Evaluate Arrays In-Place ~~~~~~~~~~~~~~~~~~~~
+*/
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Use Destructuring Assignment to Extract Values from Objects ~~~~~~~~~~~~~~~~~~~~
+*/
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+const {today, tomorrow} = HIGH_TEMPERATURES;
+// Only change code above this line
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Use Destructuring Assignment to Assign Variables from Objects ~~~~~~~~~~~~~~~~~~~~
+*/
+const HIGH_TEMPERATURES2 = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
+
+// Only change code below this line
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
+// Only change code above this line
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ UUse Destructuring Assignment to Assign Variables from Nested Objects ~~~~~~~~~~~~~~~~~~~~
+*/
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+  const {today: {low: lowToday, high: highToday2}} = LOCAL_FORECAST;
+  // Only change code above this line
