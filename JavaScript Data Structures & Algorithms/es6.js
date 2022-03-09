9b169abfeb50c51b1149593b08a7ffd8068ff649
Jax-ai <jacques@tesseract-cloud.co.za>
2022-03-09 02:59:08 +0200

    more changes

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -129,8 +129,8 @@ const LOCAL_FORECAST = {
     yesterday: { low: 61, high: 75 },
     today: { low: 64, high: 77 },
     tomorrow: { low: 68, high: 80 }
-  };
-  
-  // Only change code below this line
-  const {today: {low: lowToday, high: highToday2}} = LOCAL_FORECAST;
-  // Only change code above this line
+};
+
+// Only change code below this line
+const {today: {low: lowToday, high: highToday2}} = LOCAL_FORECAST;
+// Only change code above this line
