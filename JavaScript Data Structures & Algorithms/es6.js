b1d726363eea2e346e76d9a923da31ea46a66f22
Jax_ai <jacques@tesseract-cloud.co.za>
2023-03-06 22:49:48 +0200

    added the new method

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -178,4 +178,22 @@ const half = ({ max, min }) => (max + min) / 2.0;
 /*
 ~~~~~~~~~~~~~~~~~~~ Create Strings using Template Literals ~~~~~~~~~~~~~~~~~~~~
 */
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for(let i = 0; i < arr.length; i++) {
 
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
