100c078dbc36f302df185c7ab1a5e4d748368c04
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:06:53 +0000

    some more lessons

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -242,3 +242,26 @@ console.log(carrot.name); // Should display 'carrot'
 /*
 ~~~~~~~~~~~~~~~~~~~ Use getters and setters to Control Access to an Object ~~~~~~~~~~~~~~~~~~~~
 */
+// Only change code below this line
+class Thermostat{
+  constructor(temperature) {
+    this._temperature = temperature;
+  }
+
+  //getter
+  get temperature(){
+    return (this._temperature - 32) * 5/9;
+  }
+
+  //setter
+  set temperature(celcius) {
+    this._temperature = celcius * 9.0 / 5 + 32;
+  }
+}
+
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
