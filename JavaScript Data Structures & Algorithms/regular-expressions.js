abc62fc846574a79540aafc1d5069e9fb4e454a2
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:47:31 +0000

    .test() method

--- JavaScript Data Structures & Algorithms/regular-expressions.js
+++ JavaScript Data Structures & Algorithms/regular-expressions.js
@@ -1,3 +1,8 @@
 /*
 ~~~~~~~~~~~~~~~~~~~ Using the Test Method ~~~~~~~~~~~~~~~~~~~~
 */ 
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+
+
