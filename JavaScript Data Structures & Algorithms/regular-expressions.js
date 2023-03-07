8461a685f39d5f45814cfa226608c2288022fda9
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:58:56 +0000

    repeat match function

--- JavaScript Data Structures & Algorithms/regular-expressions.js
+++ JavaScript Data Structures & Algorithms/regular-expressions.js
@@ -36,3 +36,11 @@ let result4 = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result5 = extractStr.match(codingRegex); // Change this line
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Find More Than the First Match ~~~~~~~~~~~~~~~~~~~~
+*/
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+let result6 = twinkleStar.match(starRegex); // Change this line
