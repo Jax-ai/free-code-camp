b03a5bb2cf2d6f0b198018aca139c555406a825d
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:50:23 +0000

    added the literal stirings... easy

--- JavaScript Data Structures & Algorithms/regular-expressions.js
+++ JavaScript Data Structures & Algorithms/regular-expressions.js
@@ -6,3 +6,9 @@ let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
 
 
+/*
+~~~~~~~~~~~~~~~~~~~ Match Literal Strings ~~~~~~~~~~~~~~~~~~~~
+*/
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result2 = waldoRegex.test(waldoIsHiding);
