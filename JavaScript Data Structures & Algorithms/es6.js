979da35c5efa9eeaaee8dc830c28f78b0e57944f
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:12:36 +0000

    export function

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -273,3 +273,16 @@ temp = thermos.temperature; // 26 in Celsius
 */
   <script type="module" src="index.js"> </script>
 
+
+/*
+~~~~~~~~~~~~~~~~~~~ Use export to Share a Code Block ~~~~~~~~~~~~~~~~~~~~
+*/
+  const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+  
+  export {uppercaseString, lowercaseString};
