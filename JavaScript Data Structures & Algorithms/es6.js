8b0208c8024b2c5e18d2a37775d4e1240d46e1fc
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:15:16 +0000

    import function

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -277,12 +277,23 @@ temp = thermos.temperature; // 26 in Celsius
 /*
 ~~~~~~~~~~~~~~~~~~~ Use export to Share a Code Block ~~~~~~~~~~~~~~~~~~~~
 */
-  const uppercaseString = (string) => {
-    return string.toUpperCase();
-  }
-  
-  const lowercaseString = (string) => {
-    return string.toLowerCase()
-  }
-  
-  export {uppercaseString, lowercaseString};
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export {uppercaseString, lowercaseString};
+
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Reuse JavaScript Code Using import ~~~~~~~~~~~~~~~~~~~~
+*/
+import {uppercaseString, lowercaseString} from './string_functions.js';
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
