8af6ecf0110dcc165268ad93d6206dc3b2ae3192
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:17:28 +0000

    import everything

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -297,3 +297,18 @@ import {uppercaseString, lowercaseString} from './string_functions.js';
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Use * to Import Everything from a File ~~~~~~~~~~~~~~~~~~~~
+*/
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Use * to Import Everything from a File ~~~~~~~~~~~~~~~~~~~~
+*/
