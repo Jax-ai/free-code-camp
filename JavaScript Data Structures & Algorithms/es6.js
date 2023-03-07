4b3c8f0fc9d3b4f3316d285b257e28aab0cae4ec
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-07 17:10:13 +0000

    added the module script function

--- JavaScript Data Structures & Algorithms/es6.js
+++ JavaScript Data Structures & Algorithms/es6.js
@@ -265,3 +265,11 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
+
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Use getters and setters to Control Access to an Object ~~~~~~~~~~~~~~~~~~~~
+*/
+  <script type="module" src="index.js"> </script>
+
