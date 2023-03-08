5fd6fa49d7a64eefc481704486e4e0be684ad908
Jax_ai <jacques@tesseract-cloud.co.za>
2023-03-08 22:19:16 +0200

    next chapter

--- JavaScript Data Structures & Algorithms/regular-expressions.js
+++ JavaScript Data Structures & Algorithms/regular-expressions.js
@@ -52,3 +52,9 @@ let result6 = twinkleStar.match(starRegex); // Change this line
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
 let result7 = unRegex.test(exampleStr);
+
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Match Anything with Wildcard Period ~~~~~~~~~~~~~~~~~~~~
+*/
