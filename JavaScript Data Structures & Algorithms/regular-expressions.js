fffe6dc0d4dc4f0e07310ab28acb6fbb12f42d6c
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-03-16 20:29:34 +0000

    added the range expressions

--- JavaScript Data Structures & Algorithms/regular-expressions.js
+++ JavaScript Data Structures & Algorithms/regular-expressions.js
@@ -61,3 +61,8 @@ let result7 = unRegex.test(exampleStr);
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/ig; // Change this line
 let result8 = quoteSample.match(vowelRegex); // Change this line
+
+
+let quoteSample2 = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result9 = quoteSample2.match(alphabetRegex); // Change this line
