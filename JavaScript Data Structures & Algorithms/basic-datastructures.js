e914ae0e7224541c10ec795409be05d4c1031345
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-05-16 13:46:02 +0000

    add items

--- JavaScript Data Structures & Algorithms/basic-datastructures.js
+++ JavaScript Data Structures & Algorithms/basic-datastructures.js
@@ -12,3 +12,22 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = "j";
 // Only change code above this line
 console.log(myArray);
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Add Items to an Array with push() and unshift() ~~~~~~~~~~~~~~~~~~~~
+*/
+function mixedNumbers(arr) {
+    // Only change code below this line  
+    arr.unshift('three');
+    arr.unshift(2);
+    arr.unshift("I");
+  
+    arr.push(7);
+    arr.push("VIII");
+    arr.push(9);
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
