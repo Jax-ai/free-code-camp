736833c24ad4d4f318cabe9498a5c53d7d50ccfc
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-05-16 13:50:43 +0000

    splice

--- JavaScript Data Structures & Algorithms/basic-datastructures.js
+++ JavaScript Data Structures & Algorithms/basic-datastructures.js
@@ -46,5 +46,10 @@ function mixedNumbers(arr) {
 
 
 /*
-~~~~~~~~~~~~~~~~~~~ Remove Items from an Array with pop() and shift() ~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~ Remove Items Using splice() ~~~~~~~~~~~~~~~~~~~~
 */
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4);
+// Only change code above this line
+console.log(arr);
