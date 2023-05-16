db38e405b9a868ec6b40e7d6737d83ad52184cf6
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-05-16 13:41:54 +0000

    access array

--- JavaScript Data Structures & Algorithms/basic-datastructures.js
+++ JavaScript Data Structures & Algorithms/basic-datastructures.js
@@ -3,3 +3,12 @@
 */
 
 let yourArray = [ 'string',  false,  1,  'more',  'more']; //Change this line
+
+/*
+~~~~~~~~~~~~~~~~~~~ Access an Array's Contents Using Bracket Notation ~~~~~~~~~~~~~~~~~~~~
+*/
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "j";
+// Only change code above this line
+console.log(myArray);
