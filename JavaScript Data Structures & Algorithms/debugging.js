b1cdb58701efc535760bdcdc5b81b341b054a1f6
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-05-16 13:35:15 +0000

    infinte loop

--- JavaScript Data Structures & Algorithms/debugging.js
+++ JavaScript Data Structures & Algorithms/debugging.js
@@ -136,10 +136,11 @@ function zeroArray(m, n) {
   
 
 /*
-~~~~~~~~~~~~~~~~~~~ Catch Use of Assignment Operator Instead of Equality Operator ~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~ Prevent Infinite Loops with a Valid Terminal Condition ~~~~~~~~~~~~~~~~~~~~
 */
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
 
-
-/*
-~~~~~~~~~~~~~~~~~~~ Catch Use of Assignment Operator Instead of Equality Operator ~~~~~~~~~~~~~~~~~~~~
-*/
