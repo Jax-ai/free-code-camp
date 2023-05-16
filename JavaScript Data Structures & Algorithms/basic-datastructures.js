e7525dea7c0202ab8a3d74fb520311fd2b10a39b
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-05-16 13:55:27 +0000

    slice

--- JavaScript Data Structures & Algorithms/basic-datastructures.js
+++ JavaScript Data Structures & Algorithms/basic-datastructures.js
@@ -66,3 +66,16 @@ function htmlColorNames(arr) {
   }
   
   console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+
+/*
+~~~~~~~~~~~~~~~~~~~ Copy Array Items Using slice() ~~~~~~~~~~~~~~~~~~~~
+*/
+  function forecast(arr) {
+    // Only change code below this line
+    arr = arr.slice(2,4);
+    return arr;
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
