8cfe26d0c168ac27162437a2eb7ca34c9d90217f
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-05 15:30:34 +0200

    check if object has propery

--- JavaScript Data Structures & Algorithms/basic-datastructures.js
+++ JavaScript Data Structures & Algorithms/basic-datastructures.js
@@ -206,3 +206,104 @@ foods['strawberries'] = 27;
 // Only change code above this line
 
 console.log(foods);
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Modify an Object Nested Within an Object ~~~~~~~~~~~~~~~~~~~~
+*/
+            
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+// Only change code below this line
+userActivity.data.online = 45;
+// Only change code above this line
+
+console.log(userActivity);
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Access Property Names with Bracket Notation ~~~~~~~~~~~~~~~~~~~~
+*/
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  return foods[scannedItem];
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Use the delete Keyword to Remove Object Properties ~~~~~~~~~~~~~~~~~~~~
+*/
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries ;
+// Only change code above this line
+
+console.log(foods);
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Check if an Object has a Property ~~~~~~~~~~~~~~~~~~~~
+*/
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  // Only change code below this line
+  return userObj.hasOwnProperty("Alan") &&
+  userObj.hasOwnProperty("Jeff") &&
+  userObj.hasOwnProperty("Sarah") &&
+  userObj.hasOwnProperty("Ryan");
+  
+  // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Check if an Object has a Property ~~~~~~~~~~~~~~~~~~~~
+*/
