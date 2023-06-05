1b1c9226f07ef880f8d91bd169e6ffe63043e7bc
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-05 16:45:35 +0200

    confirm ending

--- JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
+++ JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
@@ -93,7 +93,7 @@ function largestOfFour(arr) {
                 largest = arr[i][j];
             }
         }
-        
+
         OutputArray.push(largest);
     }
 
@@ -101,3 +101,12 @@ function largestOfFour(arr) {
   }
   
   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Confirm the Ending ~~~~~~~~~~~~~~~~~~~~
+*/
+function confirmEnding(str, target) {
+    return target === str.substring(str.length - target.length);
+  }
+  
+  confirmEnding("Bastian", "n");
