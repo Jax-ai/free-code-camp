1a1b18eeae5dc32dffebbddc062001f586bbe660
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-05 16:35:35 +0200

    return largest number in multi array

--- JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
+++ JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
@@ -59,7 +59,45 @@ function factorialize(num) {
 ~~~~~~~~~~~~~~~~~~~~ Find the Longest Word in a String ~~~~~~~~~~~~~~~~~~~~
 */
 function findLongestWordLength(str) {
-    return str.length;
-  }
+    
+    let words = str.split(" "); 
+    let longest = 0
+   
+    for(let i = 0; i <= words.length -1; i++ ) {
+
+        if(words[i].length > longest) {
+            longest = words[i].length ;
+        }
+    }
+
+    return longest;
+   }
   
   findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Return Largest Numbers in Arrays ~~~~~~~~~~~~~~~~~~~~
+*/
+
+function largestOfFour(arr) {
+
+    let OutputArray = [];
+    for(let i = 0; i < arr.length; i++) {
+
+        let largest = arr[i][0];
+        for(let j = 0; j < arr[i].length; j++) {
+
+            if(arr[i][j] > largest) {
+
+                largest = arr[i][j];
+            }
+        }
+        
+        OutputArray.push(largest);
+    }
+
+    return OutputArray;
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
