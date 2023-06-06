fb8316d00d6f72d6939e93ace35982f79f3faa02
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-06 15:55:22 +0200

    splice and slice

--- JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
+++ JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
@@ -206,3 +206,14 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Slice and Splice ~~~~~~~~~~~~~~~~~~~~
+*/
+function frankenSplice(arr1, arr2, n) {
+
+  return [...arr2.slice(0,n), ...arr1, ...arr2.slice(n)];
+
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
