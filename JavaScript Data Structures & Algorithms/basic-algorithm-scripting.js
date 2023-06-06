1c6ed37c875a84d2ead19cc4e2f32cc6f2b2d48a
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-06 15:33:10 +0200

    added some more algos

--- JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
+++ JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
@@ -157,4 +157,35 @@ function truncateString(str, num) {
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
 
- 
+ /*
+~~~~~~~~~~~~~~~~~~~~ Finders Keepers ~~~~~~~~~~~~~~~~~~~~
+*/
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+ /*
+~~~~~~~~~~~~~~~~~~~~ Boo who ~~~~~~~~~~~~~~~~~~~~
+*/
+
+function booWho(bool) {  
+  return ((typeof bool) === "boolean");
+}
+
+booWho(null);
+
+
+ /*
+~~~~~~~~~~~~~~~~~~~~ Title Case a Sentence ~~~~~~~~~~~~~~~~~~~~
+*/
