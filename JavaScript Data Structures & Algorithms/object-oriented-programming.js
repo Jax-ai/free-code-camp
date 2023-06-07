cd0e9cee2d2b1413f4aebab9e0e64cd3e291b74a
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-06-07 16:51:21 +0000

    object dot notation

---
+++ JavaScript Data Structures & Algorithms/object-oriented-programming.js
@@ -0,0 +1,17 @@
+/*
+~~~~~~~~~~~~~~~~~~~~ Create a Basic JavaScript Object ~~~~~~~~~~~~~~~~~~~~
+*/
+let dog = {
+    name: "Pote",
+    numLegs: 4
+  
+  };
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Use Dot Notation to Access the Properties of an Object ~~~~~~~~~~~~~~~~~~~~
+*/let dog2 = {
+    name: "Spot",
+    numLegs: 4
+  };
+  // Only change code below this line
+  console.log(dog2.name + dog2.numLegs);
