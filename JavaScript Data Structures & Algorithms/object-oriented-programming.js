c1a408fe32fccccb24e60916db75503cc2fabcef
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-06-07 17:19:14 +0000

    prototype

--- JavaScript Data Structures & Algorithms/object-oriented-programming.js
+++ JavaScript Data Structures & Algorithms/object-oriented-programming.js
@@ -122,3 +122,19 @@ for(let property in canary) {
 
 }
 
+/*
+~~~~~~~~~~~~~~~~~~~~ Use Prototype Properties to Reduce Duplicate Code ~~~~~~~~~~~~~~~~~~~~
+
+    Properties in the prototype are shared among ALL instances
+
+*/
+function Dog(name) {
+    this.name = name;
+  }
+  
+Dog.prototype.numLegs = 4;
+  
+  
+// Only change code above this line
+let beagle = new Dog("Snoopy");
+
