b136e5d3bdb7f09e69426e95358561cb637ce0fb
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-06-07 17:02:06 +0000

    create new instance

--- JavaScript Data Structures & Algorithms/object-oriented-programming.js
+++ JavaScript Data Structures & Algorithms/object-oriented-programming.js
@@ -60,3 +60,10 @@ function Dog() {
 ~~~~~~~~~~~~~~~~~~~~ Use a Constructor to Create Objects ~~~~~~~~~~~~~~~~~~~~
 */
 
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+  }
+  // Only change code below this line
+  let hound = new Dog();
