6cf0ce547845f5210aabd8c9dd49b538b255ec96
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-06-07 17:05:22 +0000

    parameters in the constructor

--- JavaScript Data Structures & Algorithms/object-oriented-programming.js
+++ JavaScript Data Structures & Algorithms/object-oriented-programming.js
@@ -67,3 +67,17 @@ function Dog() {
   }
   // Only change code below this line
   let hound = new Dog();
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Extend Constructors to Receive Arguments ~~~~~~~~~~~~~~~~~~~~
+*/
+
+function Dog(name , color) { 
+
+    this.name = name,
+    this.color = color,
+    this.numLegs = 4
+
+}
+
+let terrier = new Dog("Pitbull terrier", "brown");
