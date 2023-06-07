76ed49d100ab31464b369dacc1a28dfb6bbd0dbf
Jacques <98424800+Jax-ai@users.noreply.github.com>
2023-06-07 16:46:42 +0000

    added the optimal solution

--- JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
+++ JavaScript Data Structures & Algorithms/basic-algorithm-scripting.js
@@ -289,3 +289,13 @@ function chunkArrayInGroups(arr, size) {
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
+// Optimal Solution:
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  while (arr.length > 0) {
+    newArr.push(arr.splice(0, size));
+  }
+  return newArr;
+}
