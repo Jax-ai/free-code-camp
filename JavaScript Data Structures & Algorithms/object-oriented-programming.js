bf7acbe65b7bacfd7e7e52c834d37671aabe3b78
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-08 21:47:58 +0200

    reset the constructor

--- JavaScript Data Structures & Algorithms/object-oriented-programming.js
+++ JavaScript Data Structures & Algorithms/object-oriented-programming.js
@@ -318,3 +318,21 @@ function Dog() { }
 // Only change code below this line
 Dog.prototype = Object.create(Animal.prototype);
 let beagle7 = new Dog();
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Reset an Inherited Constructor Property ~~~~~~~~~~~~~~~~~~~~
+*/
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+let duck3 = new Bird();
+let beagle8 = new Dog();
