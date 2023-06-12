08216c7ea34ac35f701eb4f62b8c273f55be5ebc
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-12 21:07:22 +0200

    override the inherited methods

--- JavaScript Data Structures & Algorithms/object-oriented-programming.js
+++ JavaScript Data Structures & Algorithms/object-oriented-programming.js
@@ -361,3 +361,30 @@ Dog.prototype.bark = function() {
 // Only change code above this line
 
 let beagle9 = new Dog();
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Override Inherited Methods ~~~~~~~~~~~~~~~~~~~~
+*/
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = function () {
+  return "Alas, this is a flightless bird.";
+}
+
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Override Inherited Methods ~~~~~~~~~~~~~~~~~~~~
+*/
