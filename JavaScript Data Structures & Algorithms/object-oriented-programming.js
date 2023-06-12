33a7894acc0e7a6f5e068b537a28f675051d1a62
Jax_ai <jacques@tesseract-cloud.co.za>
2023-06-12 21:23:26 +0200

    closure

--- JavaScript Data Structures & Algorithms/object-oriented-programming.js
+++ JavaScript Data Structures & Algorithms/object-oriented-programming.js
@@ -386,5 +386,39 @@ let penguin = new Penguin();
 console.log(penguin.fly());
 
 /*
-~~~~~~~~~~~~~~~~~~~~ Override Inherited Methods ~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~ Use a Mixin to Add Common Behavior Between Unrelated Objects ~~~~~~~~~~~~~~~~~~~~
 */
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+// Only change code below this line
+let glideMixin = function(obj) {
+
+  obj.glide = function () {
+
+    return "They can both glide now";
+  }
+
+}
+
+glideMixin(bird);
+glideMixin(boat);
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Use Closure to Protect Properties Within an Object from Being Modified Externally ~~~~~~~~~~~~~~~~~~~~
+*/
+function Bird() {
+  let weight = 15;
+
+  this.getWeight = function() {
+    return weight;
+  }
+
+}
