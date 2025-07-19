9b1a91d6d15f4a3bdcbcc89a737ff0f9a96a0cb0
Jax-ai <jacques@mycloudwebsite.co.za>
2025-07-19 06:41:30 +0200

    added variables and arrays

---
+++ JavaScript Algorithms and Data Structures/pyramid-generator.js
@@ -0,0 +1,50 @@
+/*
+~~~~~~~~~~~~~~~~~~~~ Build a Pyramid Generator ~~~~~~~~~~~~~~~~~~~~
+*/
+
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+console.log("#########");
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~~~
+- JavaScript has 7 primitive data types
+- Strings are immutable, which means once they are created, they cannot be changed. The variable can still be reassigned another value.
+- In JavaScript, the convention to use is camel case.
+- The default value of an uninitialized variable is undefined
+*/
+
+let character;
+let character2 = "Hello";
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Console ~~~~~~~~~~~~~~~~~~~~
+console allows you to print and view JavaScript output. 
+*/
+
+console.log(character);
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Arrays ~~~~~~~~~~~~~~~~~~~~
+- An array is a non-primitive data type 
+- Non-primitive data types differ from primitive data types in that they can hold more complex data. 
+- Primitive data types like strings and numbers can only hold one value at a time.
+- Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+*/
+let rows = [];
+console.log(rows[0]);
+
+// Getting the last element in an array
+array[array.length - 1];
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~ Methods ~~~~~~~~~~~~~~~~~~~~
+- .push() method allows you to "push" a value to the end of an array.
+- .pop() removes the last element from an array and returns that element.
+*/
+array.push(12);
+let popped = rows.pop()
